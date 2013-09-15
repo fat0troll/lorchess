62a3fe39d871afab67e3584da558e2e0d4f9949e
vonavi <ivvl82@gmail.com>
2013-09-15 12:29:47 +0300

    Use the affix plugin of Bootstrap for positioning the chess board.

--- assets/js/jq-post-load.js
+++ assets/js/jq-post-load.js
@@ -1,22 +1,9 @@
-/* Add class 'fixed' to a '#getFixed' block in order to fix its
- * position while scrolling
- */
-$(document).ready(function () {
-  var top = $('#getFixed').offset().top -
-    parseFloat($('#getFixed').css('marginTop').replace(/auto/, 0)) -
-    parseFloat($('body').css('paddingTop'));
-
-  $(window).scroll(function (event) {
-    // what the y position of the scroll is
-    var y = $(this).scrollTop();
-
-    // whether that's below the form
-    if (y >= top) {
-      // if so, ad the fixed class
-      $('#getFixed').addClass('fixed');
-    } else {
-      // otherwise remove it
-      $('#getFixed').removeClass('fixed');
+/* Fix the position of chess board while scrolling */
+$('#GameWrapper center').affix({
+  offset: {
+    top: function () {
+      return $('#GameWrapper').offset().top -
+        parseFloat($('body').css('paddingTop'))
     }
-  });
+  }
 });
